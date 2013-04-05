fe822fd5b56656722d2580ea9dff934f94f3b838
Christian Frøystad <christian@xist.no>
2013-04-05 14:54:12 +0200

    Adds sceleton QuestionPool

---
+++ Test Game/test-game/src/com/testgame/Models/QuestionPool.java
@@ -0,0 +1,18 @@
+package com.testgame.Models;
+
+import java.util.ArrayList;
+
+/**
+ * Class to hold categories and questions
+ * 
+ * Has a way of choosing a question upon request
+ *
+ */
+public class QuestionPool {
+	private ArrayList<Category> categories;
+	
+	public QuestionPool(){
+	}
+	
+	
+}
