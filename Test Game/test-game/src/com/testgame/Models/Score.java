a22752da41008206cd14c78f0d42ddca08a6c031
Andreas A. Flakstad <andflak@gmail.com>
2013-04-05 13:36:44 +0200

    minicommit

--- Test Game/test-game/src/com/testgame/Models/Score.java
+++ Test Game/test-game/src/com/testgame/Models/Score.java
@@ -4,14 +4,25 @@ public class Score {
 
 	int score;
 
+	/**
+	 * Constructor.
+	 */
 	public Score() {
 		this.score = 0;
 	}
 
+	/**
+	 * Returns the score.
+	 * @return
+	 */
 	public int getScore() {
 		return score;
 	}
 
+	/**
+	 * Updates the score.
+	 * @param score
+	 */
 	public void setScore(int score) {
 		this.score = score;
 	}
