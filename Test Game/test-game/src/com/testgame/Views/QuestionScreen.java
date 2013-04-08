bfb1eba2fa947455a095175bbbcd8114be5e4553
Andreas A. Flakstad <andflak@gmail.com>
2013-04-08 15:01:50 +0200

    Small update QuestionScreen

--- Test Game/test-game/src/com/testgame/Views/QuestionScreen.java
+++ Test Game/test-game/src/com/testgame/Views/QuestionScreen.java
@@ -17,7 +17,7 @@ public class QuestionScreen extends AbstractScreen {
 	// private final Sound wrong;
 	Question currentQuestion;
 	TextButton alt1Button, alt2Button, alt3Button, alt4Button;
-	Label questionText;
+	Label questionText, responseText;
 
 	/**
 	 * Constructor to keep a reference to the main Game class
@@ -60,19 +60,21 @@ public class QuestionScreen extends AbstractScreen {
 	public void initializeButtons() {
 		// get question
 		currentQuestion = game.getQuestionPool().getRandomQuestion();
-		
+
 		// create buttons
 		// Alternative 1
-		alt1Button = new TextButton(currentQuestion.getAlt1().getAlternativeText(), buttonStyle);
+		alt1Button = new TextButton(currentQuestion.getAlt1()
+				.getAlternativeText(), buttonStyle);
 		alt1Button.setWidth(200);
 		alt1Button.setHeight(100);
 		alt1Button
-				.setX(Gdx.graphics.getWidth() / 2 - alt1Button.getWidth() / 2);
+				.setX(Gdx.graphics.getWidth() / 3 - alt1Button.getWidth() / 3);
 		alt1Button
-				.setY((Gdx.graphics.getHeight() / 4 - alt1Button.getHeight() / 4)
+				.setY((Gdx.graphics.getHeight() / 3 - alt1Button.getHeight() / 3)
 						- Gdx.graphics.getHeight() * 0.15f);
 		// Alternative 2
-		alt2Button = new TextButton(currentQuestion.getAlt2().getAlternativeText(), buttonStyle);
+		alt2Button = new TextButton(currentQuestion.getAlt2()
+				.getAlternativeText(), buttonStyle);
 		alt2Button.setWidth(200);
 		alt2Button.setHeight(100);
 		alt2Button
@@ -93,13 +95,25 @@ public class QuestionScreen extends AbstractScreen {
 				game.setScreen(new NextPlayerScreen(game, 1));
 			}
 		});
+		alt2Button.addListener(new InputListener() {
+			public boolean touchDown(InputEvent event, float x, float y,
+					int pointer, int button) {
+				return true;
+			}
+
+			public void touchUp(InputEvent event, float x, float y,
+					int pointer, int button) {
+				game.setScreen(new NextPlayerScreen(game, 1));
+			}
+		});
 
 		// Question text field
 		LabelStyle labelStyleHeader = new LabelStyle();
 		labelStyleHeader.font = font;
 		labelStyleHeader.fontColor = new Color(0.647059f, 0.164706f, 0.164706f,
 				1.0f);
-		questionText = new Label(currentQuestion.getQuestionText(), labelStyleHeader);
+		questionText = new Label(currentQuestion.getQuestionText(),
+				labelStyleHeader);
 		questionText.setX(Gdx.graphics.getWidth() / 2 - questionText.getWidth()
 				/ 2);
 		questionText.setY(Gdx.graphics.getHeight() / 2
@@ -126,6 +140,7 @@ public class QuestionScreen extends AbstractScreen {
 	 * Remember to dispose objects.
 	 */
 	public void hide() {
+		super.hide();
 		// correct.dispose();
 		// wrong.dispose();
 	}
