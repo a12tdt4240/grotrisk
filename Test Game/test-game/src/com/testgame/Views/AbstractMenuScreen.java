023b039d6e7bfb4aa01a23acbe242fbb7c9ffaf2
Christian Frøystad <christian@xist.no>
2013-04-12 13:18:16 +0200

    Removes unused imports

--- Test Game/test-game/src/com/testgame/Views/AbstractMenuScreen.java
+++ Test Game/test-game/src/com/testgame/Views/AbstractMenuScreen.java
@@ -1,7 +1,6 @@
 package com.testgame.Views;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.NinePatch;
--- Test Game/test-game/src/com/testgame/Views/GameScreen.java
+++ Test Game/test-game/src/com/testgame/Views/GameScreen.java
@@ -10,7 +10,6 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.testgame.MyGame;
 import com.testgame.Models.Area;
 
