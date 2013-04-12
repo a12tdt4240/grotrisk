ba040b168747f91a5b57724f0e773767b26a49ce
Christian Frøystad <christian@xist.no>
2013-04-12 10:07:23 +0200

    Adds method to filter areas by player

--- Test Game/test-game/src/com/testgame/Models/Map.java
+++ Test Game/test-game/src/com/testgame/Models/Map.java
@@ -17,4 +17,23 @@ public class Map {
 	public Map(ArrayList<Area> areas) {
 		this.areas = areas;
 	}
+	
+	/**
+	 * Filters areas by player
+	 * 
+	 * @param Player
+	 * @return ArrayList<Area>
+	 */
+	public ArrayList<Area> getAreasByPlayer(Player player) {
+		ArrayList<Area> areasPlayer = new ArrayList<Area>();
+		
+		for(int i = 0; i < areas.size(); ++i) {
+			Area area = areas.get(i);
+			if(area.getOwner() == player) {
+				areasPlayer.add(area);
+			}
+		}
+		
+		return areasPlayer;
+	}
 }
