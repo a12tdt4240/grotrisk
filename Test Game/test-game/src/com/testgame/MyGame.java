4abcd8c88d564703af31e60464c827e9501af4fc
Bjørn Bråthen <bjorbrat88@gmail.com>
2013-04-18 15:20:19 +0200

    Small code formatting update.

--- Test Game/test-game/src/com/testgame/MyGame.java
+++ Test Game/test-game/src/com/testgame/MyGame.java
@@ -39,6 +39,9 @@ public class MyGame extends Game implements ApplicationListener {
 		music.setLooping(true);
 		music.play();
 
+		// Start at turn zero
+		playsCounter = 0;
+
 		// Generate question pool.
 		questionPool = new QuestionPool();
 
@@ -47,17 +50,19 @@ public class MyGame extends Game implements ApplicationListener {
 
 		// Create players.
 		players = createPlayers(2);
-		// Set player colors TO BE UPDATED WITH COLOR CHOOSER VIEW ETC
+
+		// Set player init colors so the map can be drawn.
 		players.get(0).setColor(Color.DARK_GRAY);
 		players.get(1).setColor(Color.ORANGE);
-		// Set the current player. A random player.
+
+		// Set the current player. A random player to start.
 		setCurrentPlayer(players.get((int) Math.floor(Math.random()
 				* players.size())));
-		playsCounter = 0;
 
 		// Give out initial areas
 		setInitialOwnership();
 
+		// create a DuelState for duels
 		duel = new DuelState(this);
 
 		// Create the game screen with selected map model
@@ -67,7 +72,9 @@ public class MyGame extends Game implements ApplicationListener {
 		setScreen(new MainMenuScreen(this));
 	}
 
-	// Set initial ownership
+	/**
+	 * Set initial ownership
+	 */
 	public void setInitialOwnership() {
 		map.getAreas().get(0).setOwner(getPlayers().get(0));
 		map.getAreas().get(map.getAreas().size() - 1)
@@ -171,10 +178,21 @@ public class MyGame extends Game implements ApplicationListener {
 		return currentPlayer;
 	}
 
+	/**
+	 * Returns the current players
+	 * 
+	 * @return
+	 */
 	public ArrayList<Player> getPlayers() {
 		return players;
 	}
 
+	/**
+	 * Creates a list of x Players
+	 * 
+	 * @param numberOfPlayers
+	 * @return a list of players created
+	 */
 	public ArrayList<Player> createPlayers(int numberOfPlayers) {
 		ArrayList<Player> playersCreated = new ArrayList<Player>();
 
@@ -203,21 +221,34 @@ public class MyGame extends Game implements ApplicationListener {
 		return questionPool;
 	}
 
-	@Override
-	public void dispose() {
-		music.dispose();
-	}
-
+	/**
+	 * Gets current GameScreen object
+	 * 
+	 * @return
+	 */
 	public Screen getGameScreen() {
 		return gameScreen;
 	}
 
+	/**
+	 * Increase the turn value
+	 */
 	public void increasePlaysCounter() {
 		playsCounter++;
 	}
 
+	/**
+	 * Returns current turn value
+	 * 
+	 * @return
+	 */
 	public int getPlaysCounter() {
 		return playsCounter;
+	}
+
+	@Override
+	public void dispose() {
+		music.dispose();
 	}
 
 }
